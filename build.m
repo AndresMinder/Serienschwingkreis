30fb0fc33cafb0b2fdcd95a5b6a9452f9c20bd93
Andres Minder <andres.minder@students.fhnw.ch>
2018-05-17 20:15:58 +0200

    jo

--- build.m
+++ build.m
@@ -1,17 +1,19 @@
 % Zum Sache probiere:
 clear all; close all; clc
 
-syms t ie(t) x
+syms t ie(t) x tau
+
+range = [0,1e-2];
 firstInitialValue = 0;  % Anfangsbedingungen sind = 0!
 secondInitialValue = 0;
-
-ue(t) = 2*t+10e-3+0.1/(20e-6)*int(x,x,0,t);
-% ue(t) = sin(10*t);
-
 
 R = 20;
 L = 0.1;
 C = 20e-6;
+
+% ue(t) = R*100*t + 100*L + 100/C*int(x,x,0,t);
+ue(t) = sym('5');
+
 
 figure(1)
 fplot(ue(t));
@@ -22,29 +24,28 @@ equ = diff(ue(t),t) == R*diff(ie(t),t) + L*diff(ie(t),t,2) + 1/C*ie(t);
 Die(t) = diff(ie(t),t);
 cond = [ie(0) == firstInitialValue, Die(0) == secondInitialValue];
 
-ie(t) = dsolve(equ, cond);
+% ie(t) = dsolve('Due = Die * R + L * D2ie + 1/C * ie','ie(0)=0','Die(0)=0','t');
 
 figure(2)
-fplot(@(t)ie(t), [0,1], 'g')
-%fplot(ie(t));
+fplot(@(t)ie(t), range, 'g')
 grid on;
 title 'ie(t)'
 
 uR(t) = ie(t)*R;
 uL(t) = L*diff(ie(t),t);
-uC(t) = 1/C*int(ie(t),t,0,10);
+uC(t) = 1/C*int(ie(tau),tau,0,t);
 
-% figure(3)
-% fplot(uR(t))
-% grid on;
-% title 'UR(t)'
-% figure(4)
-% fplot(uL(t))
-% grid on;
-% title 'UL(t)'
-% figure(5)
-% fplot(uC(t))
-% grid on;
-% title 'UC(t)'
+figure(3)
+fplot(@(t)uR(t),range)
+grid on;
+title 'UR(t)'
+figure(4)
+fplot(@(t)uL(t),range)
+grid on;
+title 'UL(t)'
+figure(5)
+fplot(@(t)uC(t),range)
+grid on;
+title 'UC(t)'
 
 
