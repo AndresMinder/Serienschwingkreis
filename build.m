d2c107686324964c79912adb0825f0bb91146dee
Andres Minder <andres.minder@students.fhnw.ch>
2018-05-27 21:15:08 +0200

    Force Push

--- build.m
+++ build.m
@@ -5,14 +5,14 @@ clear all; close all; clc
 % Festlegungen
 syms s t tau ue(t) ie(t)
 
-% Vom User zu bestimmende Grössen
+% Vom User zu bestimmende Grï¿½ssen
 ue(t) = 10;
 range = [0, 0.1];
 R = 20;
 L = 0.1;
 C = 20e-6;
 
-% Berechnungen der Eingangsgrössen
+% Berechnungen der Eingangsgrï¿½ssen
 % Der Eingangsstrom wurde mittels der
 % Laplace-Transformierten der Differentialgleichung der Eingangsmasche.
 % Dabei sind alle Anfangswerte gleich Null gesetzt.
@@ -20,7 +20,7 @@ Ue(s) = laplace(ue);
 Ie(s) = (s*Ue(s)*1/L)/(s^2+R/L*s+1/(C*L));
 ie(t) = ilaplace(Ie);
 
-% Plot der Eingangsgrössen
+% Plot der Eingangsgrï¿½ssen
 figure(1)
 subplot(2,1,1)
 fplot(ie, range)
@@ -52,14 +52,14 @@ grid on
 
 subplot(2,2,2)
 fplot(uL, range)
-title('Induktivitätsspannung uL(t)')
+title('Induktivitï¿½tsspannung uL(t)')
 ylabel('uL(t) [V]')
 xlabel('t [s]')
 grid on
 
 subplot(2,2,[3 4])
 fplot(uC, range)
-title('Kapazitätsspannung uC(t)')
+title('Kapazitï¿½tsspannung uC(t)')
 ylabel('uC(t) [V]')
 xlabel('t [s]')
 grid on
